67c3ab0d2358eb87890984b8a96d527b55dfbaed
Tyron Madlener <tyronx@gmail.com>
2017-11-20 15:55:41 +0100

    Basic skeleton for the HeightmapImporter tool

---
+++ HeightmapImporterModBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Server;
+
+namespace Vintagestory.HeightmapImporter
+{
+    /// <summary>
+    /// Super basic example on how to set blocks in the game
+    /// </summary>
+    public class VillageGenerator : ModBase
+    {
+        ICoreServerAPI api;
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            this.api = api;
+
+            if (api.IsModAvailable("Vintagestory.ServerMods.WorldEdit.WorldEdit"))
+            {
+                RegisterUtil.RegisterTool(api.GetMod("Vintagestory.ServerMods.WorldEdit.WorldEdit"));
+            }
+        }
+    }
+}
---
+++ HeightmapImporterTool.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+using Vintagestory.ServerMods.WorldEdit;
+
+namespace Vintagestory.HeightmapImporter
+{
+    public static class RegisterUtil
+    {
+        public static void RegisterTool(ModBase mod)
+        {
+            ((WorldEdit)mod).RegisterTool("HeightmapImporter", typeof(HeightmapImporterTool));
+        }
+    }
+
+    public class HeightmapImporterTool : ToolBase
+    {
+        public HeightmapImporterTool(WorldEditWorkspace workspace, IBlockAccessorRevertable blockAccess) : base(workspace, blockAccess)
+        {
+        }
+
+        public override Vec3i Size
+        {
+            get { return new Vec3i(1, 1, 1); }
+        }
+
+    }
+}
